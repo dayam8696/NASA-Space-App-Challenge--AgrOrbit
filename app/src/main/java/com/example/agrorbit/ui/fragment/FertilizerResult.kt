4bd03dc35b0a391e3d2f6d471b33e62282cda0de
Dayam Nadeem <101959904+dayam8696@users.noreply.github.com>
2024-10-05 14:55:50 +0530

    solved minor bugs in fertilizerresult

--- app/src/main/java/com/example/agrorbit/ui/fragment/FertilizerResult.kt
+++ app/src/main/java/com/example/agrorbit/ui/fragment/FertilizerResult.kt
@@ -14,6 +14,7 @@ class FertilizerResult : BaseFragments() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+       // return binding.root
         return binding.root
     }
 
